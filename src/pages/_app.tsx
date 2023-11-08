e22b86d6cfacc7f68ff299175b79417f7812807a
sarthak <sarthak.robo@gmail.com>
2023-11-07 21:20:13 -0500

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import { ChakraProvider } from '@chakra-ui/react'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { Analytics } from '@vercel/analytics/react';
 
 import "~/styles/globals.css";
 
@@ -10,6 +11,7 @@ config.autoAddCss = false;
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
+      <Analytics />
       <Component {...pageProps} />
     </ChakraProvider>
   );
